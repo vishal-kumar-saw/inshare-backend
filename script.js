a6c0771406a9e5dea5d506fb3e97d5d592446742
Vishal Kumar Saw <vishalkrsaw19035059@gmail.com>
2021-07-23 10:12:10 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const File = require('./models/file');
+const fs = require('fs');
+const connectDB = require('./config/db');
+connectDB();
+
+async function fetchData() {
+    const pastDate = new Date(Date.now() - 24 * 60 * 60 * 1000);
+    const files = await File.find({ createdAt: { $lt: pastDate } });
+    if (files.length) {
+        for (let file of files) {
+            try {
+                fs.unlinkSync(file.path);
+                await file.remove();
+                console.log(`successfully deleted ${file.filename}`);
+            } catch (err) {
+                console.log(`error while deleting file ${err}`);
+            }
+        }
+    }
+}
+
+fetchData().then(process.exit);
