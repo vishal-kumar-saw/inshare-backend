729051dc8b44f6e2beed94743e81f640d5f0cd8b
Vishal Kumar Saw <vishalkrsaw19035059@gmail.com>
2021-07-23 10:42:41 +0530

    add cors authorization

--- routes/files.js
+++ routes/files.js
@@ -17,6 +17,14 @@ let upload = multer({
     limit: { filesize: 1000000 * 100 }
 }).single('myfile');
 
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x - client - key, x - client - token, x - client - secret, Authorization");
+    next();
+});
+
 router.post('/', (req, res) => {
 
     //Store file
