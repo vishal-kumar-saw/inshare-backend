7df5d7a32e52ec410a757ad3147f060d0bd1172e
Vishal Kumar Saw <vishalkrsaw19035059@gmail.com>
2021-07-23 10:39:20 +0530

    add cors authorization

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 const connectDB = require('./config/db');
 connectDB();
 
 //cors
-const corsOptions = {
-    origin: process.env.ALLOWED_CLIENTS.split(',')
-}
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: process.env.ALLOWED_CLIENTS.split(',')
+// }
+// app.use(cors(corsOptions));
 //template engine
 app.use(function (req, res, next) {
     //Enabling CORS
